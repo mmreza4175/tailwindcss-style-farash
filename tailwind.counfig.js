02826b6f1559ef63c4f423b2a62cbbd1ec513283
mmreza4175 <mm.rezamousavi417511@gmail.com>
2025-10-07 16:12:44 +0330

    Initial commit

---
+++ tailwind.counfig.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    
+  ],
+  theme: {
+    extend: {}, // اگر میخوای رنگ، فونت یا اندازه اضافه کنی
+  },
+  plugins: [], // اگر پلاگین نیاز داری اینجا اضافه کن
+}
